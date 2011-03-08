0feaeb92bb5ece734b18cc9da25416215ea82cb8
Sam Cavenagh <Sam@punk.au.ad.westfield.com>
2011-03-08 14:38:16 +1100

    add disable select and js callback

--- jQuery.select2Buttons.js
+++ jQuery.select2Buttons.js
@@ -1,11 +1,11 @@
-jQuery.fn.selectButtons = function() {
+jQuery.fn.select2Buttons = function() {
   return this.each(function(selectIndex){
     var select = $(this);
     select.hide();
 
     var buttonsHtml = '<ul class="select-buttons" id="selectButtons-' + selectIndex + '">';
     select.children('option').each(function(index){
-      if ($(this).attr('disabled')){
+      if ($(this).attr('disabled') || select.attr('disabled')){
         buttonsHtml += '<li class="disabled"><span>' + $(this).html() + '</span></li>';
       }else{
         buttonsHtml += '<li><a href="#" data-select-index="' + (index + 1) + '">' + $(this).html() + '</a></li>';
@@ -16,7 +16,7 @@ jQuery.fn.selectButtons = function() {
 
     var selectButtons = $('#selectButtons-' + selectIndex);
     // Mark current selection as "picked"
-    selectButtons.find('li:nth-child(' + (select.attr("selectedIndex") + 1) + ') a').addClass('picked');
+    selectButtons.find('li:nth-child(' + (select.attr("selectedIndex") + 1) + ')').find('a, span').addClass('picked');
 
     selectButtons.find('a').click(function(e){
       event.preventDefault();
@@ -24,6 +24,7 @@ jQuery.fn.selectButtons = function() {
       selectButtons.find('a').removeClass('picked');
       $(this).addClass('picked');
       select.find('option:nth-child(' + $(this).attr('data-select-index') + ')').attr('selected', 'selected');
+      select.trigger('change');
     });
   });
 };
