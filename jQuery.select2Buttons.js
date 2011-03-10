f0903b76aacb77eab6eacecec06de3f482f9d695
Sam Cavenagh <Sam@punk.au.ad.westfield.com>
2011-03-10 16:03:22 +1100

    fix select index

--- jQuery.select2Buttons.js
+++ jQuery.select2Buttons.js
@@ -4,36 +4,37 @@ jQuery.fn.select2Buttons = function(options) {
     select.hide();
 
     var buttonsHtml = $('<div class="select2Buttons"></div>');
-
-    var addOptGroup = function(optGroupIndex, optGroup){
+    var selectIndex = 0;
+    var addOptGroup = function(optGroup){
       if (optGroup.attr('label')){
         buttonsHtml.append('<strong>' + optGroup.attr('label') + '</strong>');
       }
-      ulHtml =  $('<ul class="select-buttons">');
-      optGroup.children('option').each(function(index){
-        liHtml = $('<li></li>');
+      var ulHtml =  $('<ul class="select-buttons">');
+      optGroup.children('option').each(function(){
+        var liHtml = $('<li></li>');
         if ($(this).attr('disabled') || select.attr('disabled')){
           liHtml.addClass('disabled');
           liHtml.append('<span>' + $(this).html() + '</span>');
         }else{
-          liHtml.append('<a href="#" data-select-index="' + (index + 1) + '">' + $(this).html() + '</a>');
+          liHtml.append('<a href="#" data-select-index="' + (selectIndex + 1) + '">' + $(this).html() + '</a>');
         }
 
         // Mark current selection as "picked"
-        if((!options || !options.noDefault) && select.attr("selectedIndex") == (optGroupIndex + index)){
+        if((!options || !options.noDefault) && select.attr("selectedIndex") == selectIndex){
           liHtml.children('a, span').addClass('picked');
         }
         ulHtml.append(liHtml);
+        selectIndex++;
       });
       buttonsHtml.append(ulHtml);
     }
 
     var optGroups = select.children('optgroup');
     if (optGroups.length == 0) {
-      addOptGroup(0, select);
+      addOptGroup(select);
     }else{
-      optGroups.each(function(index){
-        addOptGroup(index, $(this));
+      optGroups.each(function(){
+        addOptGroup($(this));
       });
     }
 
@@ -42,7 +43,7 @@ jQuery.fn.select2Buttons = function(options) {
     buttonsHtml.find('a').click(function(e){
       event.preventDefault();
 
-      buttonsHtml.find('a').removeClass('picked');
+      buttonsHtml.find('a, span').removeClass('picked');
       $(this).addClass('picked');
       select.find('option:nth-child(' + $(this).attr('data-select-index') + ')').attr('selected', 'selected');
       select.trigger('change');
